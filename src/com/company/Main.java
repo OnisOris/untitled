02f113ff9918a005cfd0d96e3b89e37545b90425
OnisOris <55390311+OnisOris@users.noreply.github.com>
2020-01-06 17:37:27 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public static void main(String[] args) {
+         Object object = new Object();
+
+     //    Object dhjdj = object.clone();
+
+
+    }
+}
+
